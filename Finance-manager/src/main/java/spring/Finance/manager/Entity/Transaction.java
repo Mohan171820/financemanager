4321d4bd3698c4643c91df2ad2c85fddbf0b080c
Mohan Murali <miriyalamohanmurali74@gmail.com>
2025-12-12 20:01:26 +0530

    updated

--- Finance-manager/src/main/java/spring/Finance/manager/Entity/Transaction.java
+++ Finance-manager/src/main/java/spring/Finance/manager/Entity/Transaction.java
@@ -42,4 +42,70 @@ public class Transaction {
             this.txnDate = LocalDateTime.now();
         }
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getTxnDate() {
+        return txnDate;
+    }
+
+    public void setTxnDate(LocalDateTime txnDate) {
+        this.txnDate = txnDate;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
 }
