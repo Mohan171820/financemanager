53bcf2d8896b8ea553c1f8410e72e74588ad8c93
Mohan Murali <miriyalamohanmurali74@gmail.com>
2025-12-13 11:24:29 +0530

    update in endpoints

--- Finance-manager/src/main/java/spring/Finance/manager/Control/UserController.java
+++ Finance-manager/src/main/java/spring/Finance/manager/Control/UserController.java
@@ -7,27 +7,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")        // Common start of api endpoint
+@RequestMapping("/fm")        // Common start of api endpoint
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping                                          // It is to upload details
+    @PostMapping("/api/enterDetails")                                          // It is to upload details
     public User enterDetails(@RequestBody User user) {
         return userRepository.save(user);
     };
 
-    @GetMapping                                           // It is to get details of the user
+    @GetMapping("/api/users")                                          // It is to get details of the user
     public List<User> details(@RequestBody User user) {
         return userRepository.findAll();
     };
 
-    @GetMapping("/{userid}")                                // It gives the user details by id
+    @GetMapping("/api/{userid}")                                // It gives the user details by id
     public User getUser(@PathVariable Integer userid) {
         return userRepository.findById(userid)
                 .orElseThrow(() -> new RuntimeException("User not found"));
 };
-    @DeleteMapping("/{userid}")
+    @DeleteMapping("/api/{userid}")
     public void deleteUser(@PathVariable Integer userid) {
         userRepository.deleteById(userid);
     }
